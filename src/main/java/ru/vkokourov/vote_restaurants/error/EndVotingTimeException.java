670ca0f979bafde3e5c7821710755f758f599f40
Notebook <vkokourov@gmail.com>
2023-05-22 11:13:03 +0700

    Create EndVotingTimeException
    Add it in RestExceptionHandler like forbidden request

--- src/main/java/ru/vkokourov/vote_restaurants/error/EndVotingTimeException.java
+++ src/main/java/ru/vkokourov/vote_restaurants/error/EndVotingTimeException.java
@@ -1,7 +1,5 @@
 package ru.vkokourov.vote_restaurants.error;
 
-import java.time.LocalTime;
-
 public class EndVotingTimeException extends AppException {
     public EndVotingTimeException(String message) {
         super(message);
